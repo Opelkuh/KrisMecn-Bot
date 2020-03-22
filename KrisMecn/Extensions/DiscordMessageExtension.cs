4e6129eb273a6f8c44f263411cbb907e6add7c0e
Opelkuh <25430283+Opelkuh@users.noreply.github.com>
2020-03-22 01:19:07 +0100

    [Added] youtube search

---
+++ KrisMecn/Extensions/DiscordMessageExtension.cs
@@ -0,0 +1,53 @@
+﻿using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace KrisMecn.Extensions
+{
+    static class DiscordMessageExtension
+    {
+        public async static Task<int> PollUserAsync(this DiscordMessage msg, DiscordUser user, DiscordEmoji[] emoji, int timeout)
+        {
+            var tokenSource = new CancellationTokenSource();
+            _ = msg.CreateReactionsAsync(emoji, tokenSource.Token);
+
+            var start = DateTime.Now;
+            var timeoutSpan = TimeSpan.FromSeconds(timeout);
+            while (timeoutSpan.TotalSeconds > 0)
+            {
+                // wait for user's reaction
+                var reaction = await msg.WaitForReactionAsync(user, timeoutSpan);
+
+                // cancel if the user didn't react
+                if (reaction.TimedOut) break;
+
+                // find if the user reacted with an emoji that we expect
+                for (int i = 0; i < emoji.Length; i++)
+                {
+                    var re = reaction.Result.Emoji;
+                    if (emoji[i].Id == re.Id && emoji[i].Name == re.Name) return i;
+                }
+
+                // try again, adjust timeout for the duration that the user took to react
+                var elapsedTime = DateTime.Now - start;
+                timeoutSpan = TimeSpan.FromSeconds(timeout - elapsedTime.TotalSeconds);
+            }
+
+            tokenSource.Cancel();
+            return -1;
+        }
+
+        public async static Task CreateReactionsAsync(this DiscordMessage msg, IEnumerable<DiscordEmoji> emojis, CancellationToken cancellationToken)
+        {
+            foreach (var emoji in emojis)
+            {
+                if (cancellationToken.IsCancellationRequested) return;
+
+                await msg.CreateReactionAsync(emoji);
+            }
+        }
+    }
+}
---
+++ KrisMecn/Extensions/EmojiExtension.cs
@@ -0,0 +1,56 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace KrisMecn.Extensions
+{
+    class EmojiExtension : BaseExtension
+    {
+        public DiscordEmoji[] Numbers;
+        public DiscordEmoji Cancel;
+
+        protected override void Setup(DiscordClient client)
+        {
+            Numbers = new DiscordEmoji[]
+            {
+                DiscordEmoji.FromName(client, ":zero:"),
+                DiscordEmoji.FromName(client, ":one:"),
+                DiscordEmoji.FromName(client, ":two:"),
+                DiscordEmoji.FromName(client, ":three:"),
+                DiscordEmoji.FromName(client, ":four:"),
+                DiscordEmoji.FromName(client, ":five:"),
+                DiscordEmoji.FromName(client, ":six:"),
+                DiscordEmoji.FromName(client, ":seven:"),
+                DiscordEmoji.FromName(client, ":eight:"),
+                DiscordEmoji.FromName(client, ":nine:"),
+                DiscordEmoji.FromName(client, ":keycap_ten:"),
+            };
+            Cancel = DiscordEmoji.FromName(client, ":x:");
+        }
+
+        public DiscordEmoji FromDigit(int number)
+        {
+            if(number > 10)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return Numbers[number];
+        }
+
+        public List<DiscordEmoji> PollNumberEmojis(int numberOfOptions, int startNum = 1, bool withCancel = true)
+        {
+            var ret = new List<DiscordEmoji>(numberOfOptions);
+
+            for (int i = 0; i < numberOfOptions; i++)
+            {
+                ret.Add(FromDigit(i + startNum));
+            }
+
+            if(withCancel) ret.Add(Cancel);
+
+            return ret;
+        }
+    }
+}
