23366518d514905faef3fae16a4b8bf7e9d064d4
Jakub Opelka <opelkja15@sps-prosek.cz>
2020-03-27 21:14:53 +0100

    [Added] finished booru commands

---
+++ KrisMecn/Commands/BooruCommands.cs
@@ -0,0 +1,102 @@
+﻿using KrisMecn.Extensions;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace KrisMecn.Commands
+{
+    public class BooruCommands : BaseKrisCommandModule
+    {
+        private static DiscordColor _r34Color = new DiscordColor("#AAE5A3");
+        private static DiscordColor _danbooruColor = new DiscordColor("#0073FF");
+        private static DiscordColor _gelbooruColor = new DiscordColor("#005CD0");
+        private static DiscordColor _sankakuComplexColor = new DiscordColor("#FF761D");
+        private static DiscordColor _safebooruColor = new DiscordColor("#F0FFFF");
+
+        [
+            Command("rule34"),
+            Aliases("r34"),
+            Description("Gets a random image from rule34.xxx with the specified tags"),
+            RequireNsfw()
+        ]
+        public Task Rule34(CommandContext ctx, [RemainingText] string tags) 
+            => GetRandomImage(ctx, BooruSite.Rule34, tags, _r34Color);
+
+        [
+            Command("danbooru"),
+            Aliases("db"),
+            Description("Gets a random image from danbooru.donmai.us with the specified tags"),
+            RequireNsfw()
+        ]
+        public Task Danbooru(CommandContext ctx, [RemainingText] string tags)
+            => GetRandomImage(ctx, BooruSite.Danbooru, tags, _danbooruColor);
+
+        [
+            Command("gelbooru"),
+            Aliases("gb"),
+            Description("Gets a random image from gelbooru.com with the specified tags"),
+            RequireNsfw()
+        ]
+        public Task Gelbooru(CommandContext ctx, [RemainingText] string tags)
+            => GetRandomImage(ctx, BooruSite.Gelbooru, tags, _gelbooruColor);
+
+        [
+            Command("sankakucomplex"),
+            Aliases("sc", "sankaku"),
+            Description("Gets a random image from beta.sankakucomplex.com with the specified tags"),
+            RequireNsfw()
+        ]
+        public Task SankakuComplex(CommandContext ctx, [RemainingText] string tags)
+            => GetRandomImage(ctx, BooruSite.SankakuComplex, tags, _sankakuComplexColor);
+
+        [
+            Command("safebooru"),
+            Aliases("sb"),
+            Description("Gets a random image from safebooru.org with the specified tags")
+        ]
+        public Task Safebooru(CommandContext ctx, [RemainingText] string tags)
+            => GetRandomImage(ctx, BooruSite.Safebooru, tags, _safebooruColor);
+
+        private async Task GetRandomImage(CommandContext ctx, BooruSite site, string tags, DiscordColor? color = null)
+        {
+            var booru = ctx.Client.GetBooru();
+
+            // get string site name
+            string siteName = booru.GetSiteName(site);
+
+            // prepare embed builder
+            var eb = new DiscordEmbedBuilder()
+                .WithTitle($"Image from {siteName} with tags `{tags}`: ");
+
+            // add color
+            if (color.HasValue) eb.WithColor(color.Value);
+
+            // fetch image
+            var response = await booru.GetRandomImage(site, tags);
+
+            // don't continue if no image was found
+            if (!response.HasValue)
+            {
+                eb.WithDescription("No image found");
+                await ctx.RespondAsync(embed: eb.Build());
+                return;
+            }
+
+            var img = response.Value;
+
+            // build response embed
+            eb
+                .AddField("URL", img.fileUrl.AbsoluteUri)
+                .AddField("Tags", string.Join(' ', img.tags))
+                .WithImageUrl(img.fileUrl.AbsoluteUri)
+                .WithAuthorFooter(ctx.Member, "Requested by: ");
+
+            // add optional fields
+            if (img.score.HasValue) eb.AddField("Likes", img.score.Value.ToString(), true);
+            if (img.creation.HasValue) eb.AddField("Upload date (D.M.Y)", img.creation.Value.ToString("dd.MM.yyyy"), true);
+
+            await ctx.RespondAsync(embed: eb).ConfigureAwait(false);
+        }
+    }
+}
