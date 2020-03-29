04d2440c24c0eb219609aa0da43a88c9d2ab2216
Opelkuh <opelkja15@sps-prosek.cz>
2020-03-30 00:48:24 +0200

    [Changed] improved error handling response

---
+++ KrisMecn/Attributes/RequireVoiceChannelAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace KrisMecn.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public sealed class RequireVoiceChannelAttribute : CheckBaseAttribute
+    {
+        public override Task<bool> ExecuteCheckAsync(CommandContext ctx, bool help)
+            => Task.FromResult(ctx.Channel.Guild != null && ctx.Member?.VoiceState?.Channel != null);
+    }
+}
