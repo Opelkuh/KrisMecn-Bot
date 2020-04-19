dc096ce2e31269fd47e2eae09408cccdec07e369
Opelkuh <opelkja15@sps-prosek.cz>
2020-04-19 02:28:58 +0200

    [Added] pre-command execution logging

--- KrisMecn/Commands/BaseKrisCommandModule.cs
+++ KrisMecn/Commands/BaseKrisCommandModule.cs
@@ -1,6 +1,8 @@
 ﻿using DSharpPlus.CommandsNext;
 using System;
 using System.Threading.Tasks;
+using KrisMecn.Helpers.Extensions;
+using System.Text;
 
 namespace KrisMecn.Commands
 {
@@ -15,6 +17,27 @@ namespace KrisMecn.Commands
             {
                 Logger.Error(e);
             }
+
+            var sb = new StringBuilder();
+
+            // add basic info
+            sb.AppendFormat(
+                "`{0}` @ `{1}` requested `{2}`",
+                ctx.User.GetFullName(),
+                ctx.Guild != null ? ctx.Guild.Name : "<DMs>",
+                ctx.Command.Name
+            );
+
+            // add arguments if present
+            if (!string.IsNullOrEmpty(ctx.RawArgumentString))
+            {
+                sb.AppendFormat(
+                    " with `{0}`",
+                    ctx.RawArgumentString
+                );
+            }
+
+            Logger.Info("Command", sb.ToString());
 
             return base.BeforeExecutionAsync(ctx);
         }
