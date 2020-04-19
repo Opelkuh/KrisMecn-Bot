5f5d9f6d43d7d6c33f837c91267bb02d6274825f
Jakub Opelka <opelkja15@sps-prosek.cz>
2020-04-19 02:28:58 +0200

    [Added] pre-command execution logging

---
+++ KrisMecn/Helpers/Extensions/DiscordUserExtensions.cs
@@ -0,0 +1,10 @@
+﻿using DSharpPlus.Entities;
+
+namespace KrisMecn.Helpers.Extensions
+{
+    static class DiscordUserExtensions
+    {
+        public static string GetFullName(this DiscordUser user)
+            => $"{user.Username}#{user.Discriminator}";
+    }
+}
