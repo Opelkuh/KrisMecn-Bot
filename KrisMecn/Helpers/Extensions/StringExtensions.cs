3069e523b9a89cdaeb56119a0bb5a3329fc1e838
Opelkuh <25430283+Opelkuh@users.noreply.github.com>
2020-08-06 00:37:52 +0200

    [Added] WoW `getCharacter` command

---
+++ KrisMecn/Helpers/Extensions/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace KrisMecn.Helpers.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToFirstCharUppercase(this string str)
+        {
+            if (string.IsNullOrEmpty(str) || str.Length < 1) return str;
+            return str[0].ToString().ToUpper() + str.Substring(1);
+        }
+    }
+}
